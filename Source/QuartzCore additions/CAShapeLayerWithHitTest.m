d303ea1a512c84462f8653532b37fbdc2e938371
 王传正 <chuanzheng.wang@asiainnovations.com>
2020-01-15 23:35:25 +0800

    expand hittest

--- Source/QuartzCore additions/CAShapeLayerWithHitTest.m
+++ Source/QuartzCore additions/CAShapeLayerWithHitTest.m
@@ -7,14 +7,32 @@
  */
 @implementation CAShapeLayerWithHitTest
 
+static CGFloat targetInset = 25;
+
 - (BOOL) containsPoint:(CGPoint)p
 {
-	BOOL boundsContains = CGRectContainsPoint(self.bounds, p); // must be BOUNDS because Apple pre-converts the point to local co-ords before running the test
+	BOOL boundsContains = CGRectContainsPoint(self.expandedBounds, p);
 	
 	if( boundsContains )
 	{
-		BOOL pathContains = CGPathContainsPoint(self.path, NULL, p, false);
-		
+        /// 获取边缘大小
+        CGRect boundingBox = CGPathGetBoundingBox(self.path);
+        CGFloat targetWidth = boundingBox.size.width + targetInset * 2;
+        CGFloat targetHeight = boundingBox.size.height + targetInset * 2;
+        
+        /// 缩放比例
+        CGFloat scaleX = targetWidth / boundingBox.size.width;
+        CGFloat scaleY = targetHeight / boundingBox.size.height;
+        /// 放大
+        CGAffineTransform scaleTransform = CGAffineTransformMakeScale(scaleX, scaleY);
+        /// 转换为放大后
+        CGPathRef pathRef = CGPathCreateCopyByTransformingPath(self.path, &scaleTransform);
+        /// 平移回到原中心点
+        CGFloat x = boundingBox.size.width*scaleX - boundingBox.size.width;
+        CGFloat y = boundingBox.size.height*scaleY - boundingBox.size.height;
+        CGAffineTransform moveTransform = CGAffineTransformMakeTranslation(-x/2, -y/2);
+        CGPathRef finalPathRef = CGPathCreateCopyByTransformingPath(pathRef, &moveTransform);
+        BOOL pathContains = CGPathContainsPoint(finalPathRef, NULL, p, false);
 		if( pathContains )
 		{
 			for( CALayer* subLayer in self.sublayers )
@@ -27,4 +45,7 @@
 	return FALSE;
 }
 
+- (CGRect)expandedBounds {
+    return CGRectInset(self.bounds, -targetInset, -targetInset);
+}
 @end
