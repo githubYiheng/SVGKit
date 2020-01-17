5a4e9ffabc047099d2c87aa856a0cd22e0d400da
 王传正 <chuanzheng.wang@asiainnovations.com>
2020-01-17 14:59:20 +0800

    Test

--- Source/QuartzCore additions/CAShapeLayerWithHitTest.m
+++ Source/QuartzCore additions/CAShapeLayerWithHitTest.m
@@ -7,73 +7,69 @@
  */
 @implementation CAShapeLayerWithHitTest
 
-static CGFloat targetInset = 25;
+static CGFloat targetInset = 15;
 
 - (BOOL) containsPoint:(CGPoint)p
 {
+    BOOL boundsContains = CGRectContainsPoint(self.expend ? [self expandedBounds] : self.bounds, p); // must be BOUNDS because Apple pre-converts the point to local co-ords before running the test
     
-    if (!self.expend) {
-        BOOL boundsContains = CGRectContainsPoint(self.bounds, p); // must be BOUNDS because Apple pre-converts the point to local co-ords before running the test
+    if( boundsContains )
+    {
+        BOOL pathContains = CGPathContainsPoint(self.path, NULL, p, false);
         
-        if( boundsContains )
+        if( pathContains )
         {
-            BOOL pathContains = CGPathContainsPoint(self.path, NULL, p, false);
-            
-            if( pathContains )
+            for( CALayer* subLayer in self.sublayers )
             {
-                for( CALayer* subLayer in self.sublayers )
-                {
-                    SVGKitLogVerbose(@"...contains point, Apple will now check sublayer: %@", subLayer);
-                }
-                return TRUE;
+                SVGKitLogVerbose(@"...contains point, Apple will now check sublayer: %@", subLayer);
             }
+            return TRUE;
         }
-        return FALSE;
     }
-    
-	BOOL boundsContains = CGRectContainsPoint(self.expandedBounds, p);
-	if( boundsContains )
-	{
-        /// 获取边缘大小
-        CGRect boundingBox = CGPathGetPathBoundingBox(self.path);
-        
-        CGFloat targetWidth = CGRectGetWidth(boundingBox) + targetInset * 2;
-        CGFloat targetHeight = CGRectGetHeight(boundingBox) + targetInset * 2;
-        
-        /// 缩放比例
-        CGFloat scaleX = targetWidth / CGRectGetWidth(boundingBox);
-        CGFloat scaleY = targetHeight / CGRectGetHeight(boundingBox);
-        /// 放大
-        CGAffineTransform scaleTransform = CGAffineTransformMakeScale(scaleX, scaleY);
-        /// 转换为放大后
-        CGPathRef pathRef = CGPathCreateCopyByTransformingPath(self.path, &scaleTransform);
-        
-        CGPoint OCP = CGPointMake(CGRectGetMidX(boundingBox), CGRectGetMidY(boundingBox));
-        CGRect fBoundingBox = CGPathGetPathBoundingBox(pathRef);
-        CGPoint FCP = CGPointMake(CGRectGetMidX(fBoundingBox), CGRectGetMidY(fBoundingBox));
-        
-        /// 平移回到原中心点
-        CGFloat x = FCP.x - OCP.x;
-        CGFloat y = FCP.y - OCP.y;
-        CGAffineTransform moveTransform =  CGAffineTransformMakeTranslation(-x, -y);
-        CGPathRef finalPathRef = CGPathCreateCopyByTransformingPath(pathRef, &moveTransform);
-        BOOL pathContains = CGPathContainsPoint(finalPathRef, NULL, p, FALSE);
-        NSLog(@"layerName:%@ \npath:%@ \npathRef:%@ \nfinalPathRef:%@ \npathContains:%d \nscaleX:%f \nscaleY:%f \npointX:%f \npointY:%f \nboundingBoxX:%f \nboundingBoxY:%f \nboundingBoxW:%f \nboundingBoxH:%f \nboundingBoxX:%f \nboundingBoxY:%f \nboundingBoxW:%f \nboundingBoxH:%f \nmx:%f \nmy:%f", self.name, self.path, pathRef, finalPathRef, pathContains, scaleX, scaleY, p.x, p.y, CGRectGetMinX(boundingBox), CGRectGetMinY(boundingBox), CGRectGetWidth(boundingBox), CGRectGetHeight(boundingBox), CGRectGetMinX(fBoundingBox), CGRectGetMinY(fBoundingBox), CGRectGetWidth(fBoundingBox), CGRectGetHeight(fBoundingBox), x, y);
-        
-        CGPathRelease(pathRef);
-        CGPathRelease(finalPathRef);
-        
-        
-		if( pathContains )
-		{
-			for( CALayer* subLayer in self.sublayers )
-			{
-				SVGKitLogVerbose(@"...contains point, Apple will now check sublayer: %@", subLayer);
-			}
-			return TRUE;
-		}
-	}
-	return FALSE;
+    return FALSE;
+//	BOOL boundsContains = CGRectContainsPoint(self.expandedBounds, p);
+//	if( boundsContains )
+//	{
+//        /// 获取边缘大小
+//        CGRect boundingBox = CGPathGetPathBoundingBox(self.path);
+//
+//        CGFloat targetWidth = CGRectGetWidth(boundingBox) + targetInset * 2;
+//        CGFloat targetHeight = CGRectGetHeight(boundingBox) + targetInset * 2;
+//
+//        /// 缩放比例
+//        CGFloat scaleX = targetWidth / CGRectGetWidth(boundingBox);
+//        CGFloat scaleY = targetHeight / CGRectGetHeight(boundingBox);
+//        /// 放大
+//        CGAffineTransform scaleTransform = CGAffineTransformMakeScale(scaleX, scaleY);
+//        /// 转换为放大后
+//        CGPathRef pathRef = CGPathCreateCopyByTransformingPath(self.path, &scaleTransform);
+//
+//        CGPoint OCP = CGPointMake(CGRectGetMidX(boundingBox), CGRectGetMidY(boundingBox));
+//        CGRect fBoundingBox = CGPathGetPathBoundingBox(pathRef);
+//        CGPoint FCP = CGPointMake(CGRectGetMidX(fBoundingBox), CGRectGetMidY(fBoundingBox));
+//
+//        /// 平移回到原中心点
+//        CGFloat x = FCP.x - OCP.x;
+//        CGFloat y = FCP.y - OCP.y;
+//        CGAffineTransform moveTransform =  CGAffineTransformMakeTranslation(-x, -y);
+//        CGPathRef finalPathRef = CGPathCreateCopyByTransformingPath(pathRef, &moveTransform);
+//        BOOL pathContains = CGPathContainsPoint(finalPathRef, NULL, p, FALSE);
+//        NSLog(@"layerName:%@ \npath:%@ \npathRef:%@ \nfinalPathRef:%@ \npathContains:%d \nscaleX:%f \nscaleY:%f \npointX:%f \npointY:%f \nboundingBoxX:%f \nboundingBoxY:%f \nboundingBoxW:%f \nboundingBoxH:%f \nboundingBoxX:%f \nboundingBoxY:%f \nboundingBoxW:%f \nboundingBoxH:%f \nmx:%f \nmy:%f", self.name, self.path, pathRef, finalPathRef, pathContains, scaleX, scaleY, p.x, p.y, CGRectGetMinX(boundingBox), CGRectGetMinY(boundingBox), CGRectGetWidth(boundingBox), CGRectGetHeight(boundingBox), CGRectGetMinX(fBoundingBox), CGRectGetMinY(fBoundingBox), CGRectGetWidth(fBoundingBox), CGRectGetHeight(fBoundingBox), x, y);
+//
+//        CGPathRelease(pathRef);
+//        CGPathRelease(finalPathRef);
+//
+//
+//		if( pathContains )
+//		{
+//			for( CALayer* subLayer in self.sublayers )
+//			{
+//				SVGKitLogVerbose(@"...contains point, Apple will now check sublayer: %@", subLayer);
+//			}
+//			return TRUE;
+//		}
+//	}
+//	return FALSE;
 }
 
 - (CGRect)expandedBounds {
